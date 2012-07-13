069ea267e37cfb66537728054d96aeefebf194a9
Dean Morris <dean.a.morris@gmail.com>
2012-07-13 02:28:07 +0100

    Revert "Revert "added hud""
    This reverts commit d86ba719f6a5285f4e037f21afbfa5f1fc718e06.

---
+++ iOSDevUK/iOSDevUK/WGStatusLayer.h
@@ -0,0 +1,22 @@
+//
+//  WGStatusLayer.h
+//  iOSDevUK
+//
+//  Created by Dean Morris on 13/07/2012.
+//  Copyright 2012 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface WGStatusLayer : CCLayer {
+    
+    NSInteger health, maxHealth;
+}
+
+@property (nonatomic, readonly) NSInteger health, maxHealth;
+
++(id)setupWithData:(NSInteger)initial maxHealth:(NSInteger)max;
+-(void)adjustHealth:(NSInteger)changedAmount;
+
+@end
